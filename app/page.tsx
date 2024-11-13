0b37ac6e8c4aa3e37b61423390802dfc72351021
Alif Laksono <alif7laksono@gmail.com>
2024-11-13 16:15:58 +0700

    syifa&nabil

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,49 @@
-import Header from "./syifannabil/components/Header";
-import Hero from "./syifannabil/components/Hero";
-import Groom from "./syifannabil/components/Groom";
-import Bridge from "./syifannabil/components/Bridge";
-import Quote from "./syifannabil/components/Quote";
-import Event from "./syifannabil/components/Event";
-import Rsvp from "./syifannabil/components/Rsvp";
-import Gallery from "./syifannabil/components/Gallery";
-import Story from "./syifannabil/components/Story";
-import Gift from "./syifannabil/components/Gift";
-import Footer from "./syifannabil/components/Footer";
+import React from "react";
 
 export default function Home() {
   return (
-    <div className="">
-      <Header />
-      <Hero />
-      <Groom />
-      <Bridge />
-      <Quote />
-      <Event />
-      <Rsvp />
-      <Gallery />
-      <Story />
-      <Gift />
-      <Footer />
+    <div className="flex flex-col justify-center items-center min-h-screen p-4 ">
+      <h1 className="text-center text-2xl font-bold mb-8">
+        Jasa Pembuatan Website Undangan Pernikahan
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-5xl">
+        <div className="p-6 rounded-lg shadow-md text-center border-2 border-white hover:bg-opacity-80">
+          <h1 className="text-xl font-semibold mb-4">Modern Template</h1>
+          <button className="text-slate-500 font-medium">
+            <a
+              href="http://localhost:3000/syifannabil"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Syifa & Nabil
+            </a>
+          </button>
+        </div>
+        <div className="p-6 rounded-lg shadow-md text-center border-2 border-white hover:bg-opacity-80">
+          <h1 className="text-xl font-semibold mb-4">Modern Template</h1>
+          <button className="text-slate-500 font-medium">
+            <a
+              href="http://localhost:3000/syifannabil"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Syifa & Nabil
+            </a>
+          </button>
+        </div>
+        <div className="p-6 rounded-lg shadow-md text-center border-2 border-white hover:bg-opacity-80">
+          <h1 className="text-xl font-semibold mb-4">Modern Template</h1>
+          <button className="text-slate-500 font-medium">
+            <a
+              href="http://localhost:3000/syifannabil"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Syifa & Nabil
+            </a>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
